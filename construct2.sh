0dc2772e72bbe5797f3ab9f6adbd11b63b9a4917
Konosprod <konosprod@free.fr>
2016-05-19 13:56:07 +0200

    Utilisation du MKV plutôt
    Signed-off-by: Konosprod <konosprod@free.fr>

--- construct2.sh
+++ construct2.sh
@@ -4,7 +4,14 @@ url=$1
 
 #Download the video
 echo "Downloading"
-./you-get/you-get "$url"> out.txt
+count=0
+while : ; do
+	[[ $count != 0 ]] && break
+	./you-get/you-get "$url" > out.txt
+	echo "Download failed, retry..."
+	sleep 30
+	count=`ls -1 *.mp4 2>/dev/null | wc -l`
+done
 
 #Get the title
 title=$(cat out.txt | grep 'Title:' | sed 's/^......//')
@@ -19,8 +26,7 @@ date=$(date +%d-%m-%Y)
 
 #Extract and draw title in the video
 echo "Extract & stuff"
-sync
-./ffmpeg/ffmpeg -i "in.mp4" -ss 0 -t 30 -vf "drawtext=enable='between(t, 0, 7)':fontfile=/usr/share/fonts/truetype/fonts-japanese-mincho.ttf:textfile=./title.txt:fontcolor=white@1.0:fontsize=24:x=20:y=h-th-10:box=1:boxcolor=black@0.5:boxborderw=3,fade=out:start_time=28:duration=2" -af "afade=out:start_time=28:duration=2" -s hd720 -vcodec libx264 -crf 23 -acodec mp3 -ab 160k -ac 2 -ar 44100 -strict -2 -pix_fmt yuv420p -y -strict experimental out.mp4
+./ffmpeg/ffmpeg -i "in.mp4" -ss 0 -t 30 -vf "drawtext=enable='between(t, 0, 7)':fontfile=/usr/share/fonts/truetype/fonts-japanese-mincho.ttf:textfile=./title.txt:fontcolor=white@1.0:fontsize=24:x=20:y=h-th-10:box=1:boxcolor=black@0.5:boxborderw=3,fade=out:start_time=28:duration=2" -af "afade=out:start_time=28:duration=2" -s hd720 -vcodec libx264 -crf 23 -acodec mp3 -ab 160k -ac 2 -ar 44100 -strict -2 -pix_fmt yuv420p -y -strict experimental out.mkv
 
 #box=1:boxcolor=black@0.5:boxborderw=10
 #drawbox=y=ih/PHI:color=black@0.4:width=iw:height=48:t=max
@@ -28,15 +34,18 @@ sync
 #Concatenate with the base video
 echo "Concatenate"
 
-if [ -e ../$date.mp4 ]
+if [ -e ../$date.mkv ]
 then
-	MP4Box -force-cat -add ../$date.mp4#audio -cat out.mp4#audio a.mp4
-	MP4Box -add ../$date.mp4#video -cat out.mp4#video v.mp4
-	MP4Box -add a.mp4 -cat v.mp4 ./$date.mp4
+	mkvmerge -o ./$date.mkv ../$date.mkv + out.mkv
+	#MP4Box -force-cat -add ../$date.mp4#audio -cat out.mp4#audio a.mp4
+	#MP4Box -add ../$date.mp4#video -cat out.mp4#video v.mp4
+	#MP4Box -add a.mp4 -cat v.mp4 ./$date.mp4
 
-	mv ./$date.mp4 ../$date.mp4
+	mv ./$date.mkv ../$date.mkv
 else
-	mv out.mp4 ../$date.mp4
+	mv out.mkv ../$date.mkv
 fi
 
+echo "Cleaning"
 rm -rf *.mp4
+rm -rf *.mkv
